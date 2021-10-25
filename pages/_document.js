6d2a7c8424e2c03be29f5da8d3509cc376b16ae1
Cristian Rodriguez <eliadcrist@gmail.com>
2021-10-24 20:17:53 -0500

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,28 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
+class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en-US">
-        <Head />
-        <body>
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="shortcut icon" href="/img/brand/iafcj.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/img/brand/iafcj.png"
+          />
+        </Head>
+        <body className="text-blueGray-700 antialiased">
+          <div id="page-transition"></div>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default CustomDocument
+export default MyDocument;
